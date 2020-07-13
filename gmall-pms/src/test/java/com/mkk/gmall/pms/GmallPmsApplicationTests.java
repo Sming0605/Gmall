48e8cf13c269387c8e49a99948dbc6b9d27aa3e4
Smallming0605 <392762682@qq.com>
2020-07-13 17:38:46 +0800

    项目结构创建完成...

---
+++ gmall-pms/src/test/java/com/mkk/gmall/pms/GmallPmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkk.gmall.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallPmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
